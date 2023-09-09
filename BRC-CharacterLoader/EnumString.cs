75948d1f105b096cdbbeb88b3941eba68e18eb6d
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-09-08 23:02:20 -0700

    Fix whitespace formatting.

--- BRC-CharacterLoader/EnumString.cs
+++ BRC-CharacterLoader/EnumString.cs
@@ -8,46 +8,46 @@ namespace BRC_CharacterLoader
 		{
 			var cleanString = enumString.ToLowerInvariant().Replace(" ", "").Trim();
 
-            return cleanString switch
-            {
-                "bmx" => MoveStyle.BMX,
-                "inline" or "skates" or "inlineskates" => MoveStyle.INLINE,
-                _ => MoveStyle.SKATEBOARD,
-            };
-        }
+			return cleanString switch
+			{
+				"bmx" => MoveStyle.BMX,
+				"inline" or "skates" or "inlineskates" => MoveStyle.INLINE,
+				_ => MoveStyle.SKATEBOARD,
+			};
+		}
 
 		public static Characters ParseCharacter(string enumString)
 		{
 			var cleanString = enumString.ToLowerInvariant().Replace(" ", "").Replace(".", "").Trim();
 
-            return cleanString switch
-            {
-                "girl1" or "vinyl" => Characters.girl1,
-                "frank" => Characters.frank,
-                "ringdude" or "coil" => Characters.ringdude,
-                "blockguy" or "tryce" => Characters.blockGuy,
-                "spacegirl" or "bel" => Characters.spaceGirl,
-                "angel" or "rave" => Characters.angel,
-                "eightball" or "dotexe" => Characters.eightBall,
-                "dummy" or "solace" => Characters.dummy,
-                "dj" or "djcyber" => Characters.dj,
-                "medusa" or "eclipse" => Characters.medusa,
-                "boarder" or "deviltheory" => Characters.boarder,
-                "headman" or "faux" => Characters.headMan,
-                "prince" or "fleshprince" => Characters.prince,
-                "jetpackbossplayer" or "jetpackboss" or "rietveld" or "irene" => Characters.jetpackBossPlayer,
-                "legendface" or "felix" => Characters.legendFace,
-                "oldheadplayer" or "oldhead" => Characters.oldheadPlayer,
-                "robot" or "base" => Characters.robot,
-                "skate" or "jet" => Characters.skate,
-                "widekid" or "mesh" => Characters.wideKid,
-                "futuregirl" or "futurism" => Characters.futureGirl,
-                "puffergirl" or "rise" => Characters.pufferGirl,
-                "bungirl" or "shine" => Characters.bunGirl,
-                "eightballboss" or "dotexeboss" => Characters.eightBallBoss,
-                "legendmetalhead" or "redfelix" => Characters.legendMetalHead,
-                _ => Characters.metalHead,
-            };
-        }
+			return cleanString switch
+			{
+				"girl1" or "vinyl" => Characters.girl1,
+				"frank" => Characters.frank,
+				"ringdude" or "coil" => Characters.ringdude,
+				"blockguy" or "tryce" => Characters.blockGuy,
+				"spacegirl" or "bel" => Characters.spaceGirl,
+				"angel" or "rave" => Characters.angel,
+				"eightball" or "dotexe" => Characters.eightBall,
+				"dummy" or "solace" => Characters.dummy,
+				"dj" or "djcyber" => Characters.dj,
+				"medusa" or "eclipse" => Characters.medusa,
+				"boarder" or "deviltheory" => Characters.boarder,
+				"headman" or "faux" => Characters.headMan,
+				"prince" or "fleshprince" => Characters.prince,
+				"jetpackbossplayer" or "jetpackboss" or "rietveld" or "irene" => Characters.jetpackBossPlayer,
+				"legendface" or "felix" => Characters.legendFace,
+				"oldheadplayer" or "oldhead" => Characters.oldheadPlayer,
+				"robot" or "base" => Characters.robot,
+				"skate" or "jet" => Characters.skate,
+				"widekid" or "mesh" => Characters.wideKid,
+				"futuregirl" or "futurism" => Characters.futureGirl,
+				"puffergirl" or "rise" => Characters.pufferGirl,
+				"bungirl" or "shine" => Characters.bunGirl,
+				"eightballboss" or "dotexeboss" => Characters.eightBallBoss,
+				"legendmetalhead" or "redfelix" => Characters.legendMetalHead,
+				_ => Characters.metalHead,
+			};
+		}
 	}
 }
