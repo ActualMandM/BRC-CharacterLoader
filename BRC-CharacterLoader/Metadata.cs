0390144eddf702df30c1835aaa8bb7ba7a825aed
Michael <15317421+ActualMandM@users.noreply.github.com>
2023-09-09 14:49:43 -0700

    Switch to spaces.

--- BRC-CharacterLoader/Metadata.cs
+++ BRC-CharacterLoader/Metadata.cs
@@ -2,33 +2,33 @@ using System.Collections.Generic;
 
 namespace BRC_CharacterLoader
 {
-	public class Metadata
-	{
-		public string bundleName { get; set; } = string.Empty;
-		public string prefabName { get; set; } = string.Empty;
-		public string charaName { get; set; } = string.Empty;
-		public int defaultOutfit { get; set; } = 0;
-		public bool canBlink { get; set; } = false;
+    public class Metadata
+    {
+        public string bundleName { get; set; } = string.Empty;
+        public string prefabName { get; set; } = string.Empty;
+        public string charaName { get; set; } = string.Empty;
+        public int defaultOutfit { get; set; } = 0;
+        public bool canBlink { get; set; } = false;
         public string moveStyle { get; set; } = string.Empty;
-		public string voiceBase { get; set; } = string.Empty;
-		public int freeStyle { get; set; } = 0;
-		public int bounce { get; set; } = 0;
-		public List<Outfit>? outfits { get; set; }
-		public Graffiti? graffiti { get; set; }
-	}
+        public string voiceBase { get; set; } = string.Empty;
+        public int freeStyle { get; set; } = 0;
+        public int bounce { get; set; } = 0;
+        public List<Outfit>? outfits { get; set; }
+        public Graffiti? graffiti { get; set; }
+    }
 
-	public class Outfit
-	{
-		public string outfitMaterial { get; set; } = string.Empty;
-		public string outfitName { get; set; } = string.Empty;
-	}
+    public class Outfit
+    {
+        public string outfitMaterial { get; set; } = string.Empty;
+        public string outfitName { get; set; } = string.Empty;
+    }
 
-	public class Graffiti
-	{
-		public string graffitiBase { get; set; } = string.Empty;
-		public string graffitiName { get; set; } = string.Empty;
-		public string graffitiArtist { get; set; } = string.Empty;
-		public string graffitiMaterial { get; set; } = string.Empty;
-		public string graffitiTexture { get; set; } = string.Empty;
-	}
+    public class Graffiti
+    {
+        public string graffitiBase { get; set; } = string.Empty;
+        public string graffitiName { get; set; } = string.Empty;
+        public string graffitiArtist { get; set; } = string.Empty;
+        public string graffitiMaterial { get; set; } = string.Empty;
+        public string graffitiTexture { get; set; } = string.Empty;
+    }
 }
--- BRC-CharacterLoader/Plugin.cs
+++ BRC-CharacterLoader/Plugin.cs
@@ -10,104 +10,104 @@ using UnityEngine;
 
 namespace BRC_CharacterLoader
 {
-	[BepInPlugin("com.MandM.BRC-CharacterLoader", "BRC-CharacterLoader", "0.9.9")]
-	[BepInDependency("com.Viliger.CharacterAPI", "0.8.0")]
+    [BepInPlugin("com.MandM.BRC-CharacterLoader", "BRC-CharacterLoader", "0.9.9")]
+    [BepInDependency("com.Viliger.CharacterAPI", "0.8.0")]
 
-	public class Plugin : BaseUnityPlugin
-	{
-		[DllImport("User32.dll", CharSet = CharSet.Unicode)]
-		private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
+    public class Plugin : BaseUnityPlugin
+    {
+        [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+        private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, uint uType);
 
-		public void Awake()
-		{
-			string charaPath = Path.Combine(Path.GetDirectoryName(Info.Location), "Characters");
+        public void Awake()
+        {
+            string charaPath = Path.Combine(Path.GetDirectoryName(Info.Location), "Characters");
 
-			if (!Directory.Exists(charaPath))
-				Directory.CreateDirectory(charaPath);
+            if (!Directory.Exists(charaPath))
+                Directory.CreateDirectory(charaPath);
 
-			foreach (string folder in Directory.GetDirectories(charaPath))
-			{
-				string folderName = new DirectoryInfo(folder).Name;
-				bool isPlugin = false;
+            foreach (string folder in Directory.GetDirectories(charaPath))
+            {
+                string folderName = new DirectoryInfo(folder).Name;
+                bool isPlugin = false;
 
-				try
-				{
-					// Detect if user has installed a BepInEx plugin
-					foreach (string file in Directory.GetFiles(folder))
-					{
-						if (Path.GetExtension(file) == ".dll")
-						{
-							Logger.LogError($"[{folderName}] BepInEx plugin detected.");
-							MessageBox(IntPtr.Zero, $"{folderName} is a BepInEx plugin, it will not load through BRC-CharacterLoader.\n\nPlease install it in the proper location: \"BombRushCyberfunk\\BepInEx\\plugins\"", "BRC-CharacterLoader", 0);
-							isPlugin = true;
-						}
-					}
+                try
+                {
+                    // Detect if user has installed a BepInEx plugin
+                    foreach (string file in Directory.GetFiles(folder))
+                    {
+                        if (Path.GetExtension(file) == ".dll")
+                        {
+                            Logger.LogError($"[{folderName}] BepInEx plugin detected.");
+                            MessageBox(IntPtr.Zero, $"{folderName} is a BepInEx plugin, it will not load through BRC-CharacterLoader.\n\nPlease install it in the proper location: \"BombRushCyberfunk\\BepInEx\\plugins\"", "BRC-CharacterLoader", 0);
+                            isPlugin = true;
+                        }
+                    }
 
-					if (isPlugin)
-						continue;
+                    if (isPlugin)
+                        continue;
 
-					string jsonText = File.ReadAllText(folder + "/metadata.json");
-					List<Metadata>? modCharacters = JsonSerializer.Deserialize<List<Metadata>>(jsonText);
+                    string jsonText = File.ReadAllText(folder + "/metadata.json");
+                    List<Metadata>? modCharacters = JsonSerializer.Deserialize<List<Metadata>>(jsonText);
 
-					if (modCharacters == null)
-					{
-						Logger.LogError($"[{folderName}] JSON data either doesn't exist or is invalid.");
-						continue;
-					}
+                    if (modCharacters == null)
+                    {
+                        Logger.LogError($"[{folderName}] JSON data either doesn't exist or is invalid.");
+                        continue;
+                    }
 
-					foreach (Metadata charaData in modCharacters)
-					{
-						AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(folder, charaData.bundleName));
+                    foreach (Metadata charaData in modCharacters)
+                    {
+                        AssetBundle bundle = AssetBundle.LoadFromFile(Path.Combine(folder, charaData.bundleName));
 
-						ModdedCharacterConstructor chara = new ModdedCharacterConstructor();
-						chara.characterName = charaData.charaName;
-						chara.characterPrefab = bundle.LoadAsset<GameObject>(charaData.prefabName);
-						chara.defaultOutfit = Math.Abs(charaData.defaultOutfit) % 4;
-						chara.canBlink = charaData.canBlink;
-						chara.defaultMoveStyle = EnumString.ParseMoveStyle(charaData.moveStyle);
-						chara.freestyleType = (ModdedCharacterConstructor.FreestyleType)(Math.Abs(charaData.freeStyle) % 19);
-						chara.bounceType = (ModdedCharacterConstructor.BounceType)(Math.Abs(charaData.bounce) % 16);
+                        ModdedCharacterConstructor chara = new ModdedCharacterConstructor();
+                        chara.characterName = charaData.charaName;
+                        chara.characterPrefab = bundle.LoadAsset<GameObject>(charaData.prefabName);
+                        chara.defaultOutfit = Math.Abs(charaData.defaultOutfit) % 4;
+                        chara.canBlink = charaData.canBlink;
+                        chara.defaultMoveStyle = EnumString.ParseMoveStyle(charaData.moveStyle);
+                        chara.freestyleType = (ModdedCharacterConstructor.FreestyleType)(Math.Abs(charaData.freeStyle) % 19);
+                        chara.bounceType = (ModdedCharacterConstructor.BounceType)(Math.Abs(charaData.bounce) % 16);
 
-						if (!String.IsNullOrEmpty(charaData.voiceBase))
-							chara.tempAudioBase = EnumString.ParseCharacter(charaData.voiceBase);
-						else
-							chara.audioClips = bundle.LoadAllAssets<AudioClip>().ToList();
+                        if (!String.IsNullOrEmpty(charaData.voiceBase))
+                            chara.tempAudioBase = EnumString.ParseCharacter(charaData.voiceBase);
+                        else
+                            chara.audioClips = bundle.LoadAllAssets<AudioClip>().ToList();
 
-						if (charaData.outfits != null)
-						{
-							foreach (Outfit outfitData in charaData.outfits)
-								chara.AddOutfit(bundle.LoadAsset<Material>(outfitData.outfitMaterial), outfitData.outfitName);
-						}
-						else
-						{
-							Logger.LogError($"[{folderName}] {charaData.charaName} is missing outfit data.");
-						}
+                        if (charaData.outfits != null)
+                        {
+                            foreach (Outfit outfitData in charaData.outfits)
+                                chara.AddOutfit(bundle.LoadAsset<Material>(outfitData.outfitMaterial), outfitData.outfitName);
+                        }
+                        else
+                        {
+                            Logger.LogError($"[{folderName}] {charaData.charaName} is missing outfit data.");
+                        }
 
-						if (charaData.graffiti != null)
-						{
-							if (!String.IsNullOrEmpty(charaData.graffiti.graffitiBase))
-							{
-								chara.personalGraffitiBase = EnumString.ParseCharacter(charaData.graffiti.graffitiBase);
-							}
-							else
-							{
-								chara.AddPersonalGraffiti(
-									charaData.graffiti.graffitiName,
-									charaData.graffiti.graffitiArtist,
-									bundle.LoadAsset<Material>(charaData.graffiti.graffitiMaterial),
-									bundle.LoadAsset<Texture>(charaData.graffiti.graffitiTexture)
-								);
-							}
-						}
+                        if (charaData.graffiti != null)
+                        {
+                            if (!String.IsNullOrEmpty(charaData.graffiti.graffitiBase))
+                            {
+                                chara.personalGraffitiBase = EnumString.ParseCharacter(charaData.graffiti.graffitiBase);
+                            }
+                            else
+                            {
+                                chara.AddPersonalGraffiti(
+                                    charaData.graffiti.graffitiName,
+                                    charaData.graffiti.graffitiArtist,
+                                    bundle.LoadAsset<Material>(charaData.graffiti.graffitiMaterial),
+                                    bundle.LoadAsset<Texture>(charaData.graffiti.graffitiTexture)
+                                );
+                            }
+                        }
 
-						chara.CreateModdedCharacter();
-					}
-				}
-				catch (Exception ex)
-				{
-					Logger.LogError($"[{folderName}] Exception: " + ex.Message);
-				}
-			}
-		}
-	}
+                        chara.CreateModdedCharacter();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogError($"[{folderName}] Exception: " + ex.Message);
+                }
+            }
+        }
+    }
 }
